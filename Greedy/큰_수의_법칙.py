dad1db8caa7b171e6e546e348bc6301552bbaeb9
SummerToday <88650436+SummerToday@users.noreply.github.com>
2024-08-28 02:56:36 +0000

    큰 수의 법칙 - 1

---
+++ Greedy/큰_수의_법칙.py
@@ -0,0 +1,16 @@
+n, m, k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+count = m // (k+1) * k + m % (k+1)
+sum = first * count + second * (m - count)
+
+print(sum)
+
+            
+            
