906b16afd1adaade54c0b2ed2f25db6d8857d292
SummerToday <kom347@naver.com>
2025-01-05 20:42:59 +0900

    시각 수정

--- 구현/시각.py
+++ 구현/시각.py
@@ -11,19 +11,3 @@ for l in range(a+1):
                 count += 1
 
 print(count)
-
-
-
-'''
-for i in range(x):
-    if i % 3 == 0:
-        countX += 1
-    for k in range(y):
-        if k % 3 == 0:
-            if (count)
-            countY += 1
-        for p in range(y):  
-            if p % 3 == 0:
-                countZ += 1
-'''
- 
